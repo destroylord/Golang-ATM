475c968cd763776d2b7a8093d71e3635133eac44
destroylord <masapin68@gmail.com>
2022-08-29 06:56:50 +0700

    penarikan saldo

--- go.mod
+++ go.mod
@@ -1,10 +1,3 @@
 module example.com/m
 
 go 1.18
-
-require (
-	github.com/cockroachdb/apd v1.1.0 // indirect
-	github.com/leekchan/accounting v1.0.0 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
-	github.com/shopspring/decimal v0.0.0-20180709203117-cd690d0c9e24 // indirect
-)
--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/leekchan/accounting"
 )
 
 // global variable
@@ -12,11 +10,8 @@ var totalSaldo, sisaSaldo, setoranSaldo, tarikSaldo int
 
 func main() {
 
-	// Converet To Currency
-	ac := accounting.Accounting{Symbol: "Rp", Precision: 2}
-
 	// Default set Money
-	var saldo string = ac.FormatMoney(1000000)
+	saldo := 1000000
 
 	totalSaldo = 50000
 	// saldoTotal := saldo + totalSaldo
@@ -34,6 +29,7 @@ func main() {
 		fmt.Print("Kode yang anda masukkan salah, silahkan input dengan benar \n")
 		main()
 	} else {
+
 		Template()
 
 		switch menu {
@@ -42,22 +38,61 @@ func main() {
 			fmt.Printf("NamaPengguna: %s \n", namaPengguna)
 			fmt.Printf("Sebelumnya: %v \n", saldo)
 			fmt.Print("<- Kembali")
-			// fmt.Printf("SALDO REKENING ANDA: Rp.%d \n", saldoTotal)
 
 		case "2":
 			fmt.Printf("Saldo saat ini: %v \n", totalSaldo)
 			fmt.Println("2. Menu Setor Tunai")
-			fmt.Printf("Masukkan nominal: ")
-			fmt.Scan(&setoranSaldo)
-			operatorSetoran(totalSaldo, setoranSaldo)
-			// fmt.Print(totalSaldo)
-			// Template()
+			fmt.Print("Kirim Nominal :")
+			var nominalSetor int
+			fmt.Scan(&nominalSetor)
+			totalSetoranSaldo := totalSaldo + nominalSetor
+			fmt.Printf("Setoran kamu telah ditambahkan, saldo saat ini: %d", totalSetoranSaldo)
+
 		case "3":
-			fmt.Println("3. Menu Tarik Tunai")
-			// fmt.Println("Saldo Awal : ", saldoTotal)
-			// OperatorTarikTunai(saldoTotal)
-			// fmt.Printf("Masukkan nominal setoran: %b \n", tarikSaldo)
-			// fmt.Scan(&tarikSaldo)
+			fmt.Println("3. Menu Tarik Tunai \n")
+			fmt.Println("1. 50000")
+			fmt.Println("2. 100000")
+			fmt.Println("3. 200000")
+			fmt.Println("4. 500000")
+			fmt.Println("5. 1000000")
+			fmt.Println("6. Jumlah lainnya ")
+
+			// dibikin array kemudian di looping
+			var menus = map[int]int{
+				1: 50000,
+				2: 100000,
+				3: 200000,
+				4: 500000,
+				5: 1000000,
+			}
+
+			// var nominalPenarikan int
+			// fmt.Scan(nominalPenarikan)
+			// fmt.Printf("Silahkan masukkan nominal yang akan anda tarik: %d", nominalPenarikan)
+
+			for key := range menus {
+
+				fmt.Printf("Silahkan masukkan menu: ")
+				fmt.Scan(&key)
+
+				if key >= len(menus) {
+					var changeNominal int
+					fmt.Printf("Masukkan nominal yang anda inginkan: ")
+					fmt.Scan(&changeNominal)
+
+					totalPenarikanSaldoChange := saldo - changeNominal
+					fmt.Printf("Sisa saldo anda setelah penarikan adalah :%d", totalPenarikanSaldoChange)
+					break
+				} else {
+
+					totalPenarikanSaldo := saldo - menus[key]
+					fmt.Printf("Saldo penarikan saat ini: %d", totalPenarikanSaldo)
+					break
+
+				}
+
+			}
+
 		default:
 			fmt.Println("Tidak terdapat menu")
 			Template()
@@ -74,14 +109,8 @@ func Template() {
 	fmt.Println("4. Ubah Pin")
 	fmt.Print("Silahkan Masukkan menu: ")
 	fmt.Scan(&menu)
-	fmt.Println("\033[2J")
+	// fmt.Println("\033[2J")
 	// fmt.Print("======= Copyright by Dafrin =======")
-}
-
-func operatorSetoran(totalSaldo, setoranSaldo int) int {
-	var value int = totalSaldo + setoranSaldo
-	fmt.Printf("Saldo anda: %b", value)
-	return value
 }
 
 func OperatorTarikTunai(totalSaldo int) int {
